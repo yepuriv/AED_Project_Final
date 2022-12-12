51069c83c3dcb80cd0afa4870a7a9b77938328d1
Akshit <kallepalli.l@northeastern.edu>
2022-12-11 20:04:23 -0500

    Work Queue

--- Clothes/src/Business/WorkQueue/WorkQueue.java
+++ Clothes/src/Business/WorkQueue/WorkQueue.java
@@ -4,10 +4,23 @@
  */
 package Business.WorkQueue;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author akshitvarma
  */
 public class WorkQueue {
+     private ArrayList<WorkRequest> workRequestList;
+
+    public WorkQueue() {
+        workRequestList = new ArrayList();
+    }
+
+    public ArrayList<WorkRequest> getWorkRequestList() {
+        return workRequestList;
+    }
     
 }
+
+    
