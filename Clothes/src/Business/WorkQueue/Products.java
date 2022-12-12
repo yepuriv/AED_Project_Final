4d836bf8acd0129d1defafe656ea1fb3145208db
Varun Kumar <yepuri.v@northeastern.edu>
2022-12-11 20:28:29 -0500

    Tried to remove the added exrta featires and completed Workqueue

--- Clothes/src/Business/WorkQueue/Products.java
+++ Clothes/src/Business/WorkQueue/Products.java
@@ -9,5 +9,41 @@ package Business.WorkQueue;
  * @author akshitvarma
  */
 public class Products {
+    private String productType;
+    private String productName;
+    private int quantity;
+    
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    @Override
+    public String toString() {
+        return productType;
+    }
+    
     
 }
+
+
